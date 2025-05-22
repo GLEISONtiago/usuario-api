3fdd15b3acf639361feaba7d30b9a598683e52de
GLEISON TIAGO MARTINS DE ARAUJO <gleisontiago@Outlook.com>
2025-05-22 14:10:20 -0300

    Delete usuario-api/usuario-api directory

--- usuario-api/usuario-api/src/main/java/com/exemplo/usuarioapi/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.usuarioapi.repository;
-
-import com.exemplo.usuarioapi.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
